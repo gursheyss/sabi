1dd5e872e7a7608c3625baaa9c2c6f36da9f1e0f
gursheyss <rkwarya@gmail.com>
2025-02-11 17:11:18 -0800

    add sidebar provider back

--- packages/www/app/layout.tsx
+++ packages/www/app/layout.tsx
@@ -29,10 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}
       >
-        <SidebarProvider>
-          {/* <AppSidebar /> */}
-          {children}
-        </SidebarProvider>
+        {children}
       </body>
     </html>
   );
