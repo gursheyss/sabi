6b8e9fb3d54b46cfa72ba6db5bc57ac9cacef00b
gursheyss <rkwarya@gmail.com>
2025-02-16 12:14:59 -0800

    update trusted origins again

--- packages/www/lib/auth.ts
+++ packages/www/lib/auth.ts
@@ -8,7 +8,9 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
-  trustedOrigins: ["http://localhost:8080", "https://www.app.heysabi.com", "http://localhost:3000"],
+  trustedOrigins: [
+    "https://www.app.heysabi.com",
+  ],
   database: drizzleAdapter(db, {
     provider: "pg"
   }),
